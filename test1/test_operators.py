2dac268fc22933aa24978170dc268a4573052258
kadudulanavya <navyanavya4500@gmail>
2026-07-21 17:11:20 +0530

    my nineth own commit

--- test1/test_operators.py
+++ test1/test_operators.py
@@ -1,4 +1,4 @@
-from src1.maths import add,sub,mul
+from src1.maths import add,sub,mul,div
 def test_add():
     assert add(2,3)==5
     assert add(3,3)==6
@@ -11,5 +11,7 @@ def test_mul():
     assert mul(2,3)==6
     assert mul(3,3)==9
     assert mul(4,3)==12
+def test_div():
+    assert div(2,2)==1
 
 
