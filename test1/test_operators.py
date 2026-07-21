aa2cdfdb65e9c2acc30a79ba2831a756abd061be
kadudulanavya <navyanavya4500@gmail>
2026-07-21 16:39:50 +0530

    merge changes

--- test1/test_operators.py
+++ test1/test_operators.py
@@ -1,4 +1,4 @@
-from src1.maths import add,sub
+from src1.maths import add,sub,mul
 def test_add():
     assert add(2,3)==5
     assert add(3,3)==6
@@ -7,3 +7,9 @@ def test_sub():
     assert sub(2,3)==-1
     assert sub(3,3)==0
     assert sub(4,3)==1
+def test_mul():
+    assert mul(2,3)==6
+    assert mul(3,3)==9
+    assert mul(4,3)==12
+
+
